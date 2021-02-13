3590378a4eed7ec7d112fa95976ec8f168d2d567
ggichuru <ggichuru.dev@gmail.com>
2021-02-14 01:36:47 +0300

    add: Bootstrap

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['portfolio/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
